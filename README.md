5f28f6c6a8cd36feaef1759bb165fe99aa5d1b05
Rupesh Tiwari <roopkt@gmail.com>
2024-03-08 09:49:44 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
+
+```bash
+GET _cat/allocation?format=json
+```
+
+![allocation-os](allocation-aos.png)
+
+```py
+python3 ./allocation.py
+```
+
 ![allocation](allocation.png)
 
-![shards](shards.png)
+
+
+```bash
+GET _cat/shards?format=json
+```
+
+![shards-aos](shards-aos.png)
+
+```py
+python3 ./shards.py
+```
+
+ ![shards](shards.png)
--- shards.py
+++ shards.py
@@ -8,6 +8,9 @@ with open("./shards.json", "r") as file:
 # Count primary and replica shards
 primary_count = sum(1 for shard in shards if shard["prirep"] == "p")
 replica_count = sum(1 for shard in shards if shard["prirep"] == "r")
+total_count = primary_count + replica_count
 
 print(f"Primary Shards: {primary_count}")
 print(f"Replica Shards: {replica_count}")
+print(f"------------------------")
+print(f"Total Shards: {total_count}")
